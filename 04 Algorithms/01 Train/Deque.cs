961e65519431f6543aaa3b79bc39ace2465130ae
elitam <elinski@gmail.com>
2018-08-29 14:39:01 +0300

    first commit

---
+++ 04 Algorithms/01 Train/Deque.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Train
+{
+
+    public class Deque<T> where T : Train
+    {
+        private static int defaultCapacity = 16;
+
+        private Stack<T> passengerTrains;
+        private Stack<T> freightTrains;
+        private Stack<T> history;
+
+        public Deque() : this(defaultCapacity) { }
+        public Deque(int capacity)
+        {
+            this.passengerTrains = new Stack<T>(capacity);
+            this.freightTrains = new Stack<T>(capacity);
+            this.history = new Stack<T>();
+            this.Capacity = capacity;
+        }
+        public Deque(IEnumerable<T> collection)
+             : this(collection.Count())
+        {
+            //създава дека с капацитет съответстващ на посочената колекция и прехвърля елементите от колекцията в дека
+        }
+        public int Capacity; //показва капацитета
+        public int Count; //показва броят елементи
+        public void AddFront(T item)
+        {
+            //добавя елемент отпред
+        }
+        public void AddBack(T item)
+        {
+            //добавя елемент отзад
+        }
+        public T RemoveFront()
+        {
+            //връща и премахва елемента отпред
+        }
+        public T RemoveBack()
+        {
+            //връща и премахва елемента отзад
+        }
+        public T GetFront()
+        {
+            //връща, без да премахва, елемента отпред
+        }
+        public T GetBack()
+        {
+            //връща, без да премахва, елемента отзад
+        }
+    }
+
+}
---
+++ 04 Algorithms/01 Train/Train.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Train
+{
+    class Train
+    {
+        private int number;
+        private string name;
+        private string type;
+        private int cars;
+
+        public Train(int number, string name, string type, int cars)
+        {
+            this.number = number;
+            this.name = name;
+            this.type = type;
+            this.cars = cars;
+        }
+
+        public int Number
+        {
+            get { return number; }
+            set
+            {
+                number = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Type
+        {
+            get { return type; }
+            set
+            {
+                type = value;
+            }
+        }
+        public int Cars
+        {
+            get { return cars; }
+            set
+            {
+                cars = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            // 14545 Varna P 4
+            return $"{this.Number} {this.Name} {this.Type} {this.Cars}";
+        }
+    }
+}
